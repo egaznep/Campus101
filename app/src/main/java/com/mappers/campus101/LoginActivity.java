6e7ba91d637fd226249aa23bb83f4eb1593f2b0c
KaanOzk <kaan.ozkara@icloud.com>
2016-04-23 01:31:42 +0300

    Back to Good

--- app/src/main/java/com/mappers/campus101/LoginActivity.java
+++ app/src/main/java/com/mappers/campus101/LoginActivity.java
@@ -1,10 +1,7 @@
 package com.mappers.campus101;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,7 +29,6 @@ public class LoginActivity extends AppCompatActivity {
         //Calling the login function
 
         LoginManager.sendLoginRequest( String.valueOf(editTextstudentID.getText()), editTextPassword.getText().toString());
-        Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
-        startActivity(intent);
+
     }
 }
