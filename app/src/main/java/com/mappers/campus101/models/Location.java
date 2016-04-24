8cb8800e339f53718d8d5186d3d859863c290068
necemd <nurecemdilek@hotmail.com>
2016-04-25 00:25:26 +0300

    Location class implements Locatable

--- app/src/main/java/com/mappers/campus101/models/Location.java
+++ app/src/main/java/com/mappers/campus101/models/Location.java
@@ -1,8 +1,50 @@
 package com.mappers.campus101.models;
 
 /**
- * @author Kadir Can Çelik
+ * @author Nur Ecem Dilek
  * @date 24.4.2016
  */
-public class Location {
+public class Location implements Locatable
+{
+    // intance variables
+    private double longitude;
+    private double latitude;
+
+    // constructor
+    public Location ( double longitude, double latitude)
+    {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    // set method
+    public void setLocation ( double longitude, double latitude)
+    {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    // getters
+    public double getLongitude()
+    {
+        return longitude;
+    }
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+
+    // implementation of method from Locatable interface
+    public boolean isAtLocation( Location loc)
+    {
+        return loc.getLongitude() == this.longitude && loc.getLatitude() == this.latitude;
+    }
+
+    // updates the longitude and tatitude
+    private void updateLocation( double longitude, double latitude)
+    {
+        setLocation( longitude, latitude);
+    }
+
 }
