599cc19d6f104619119c19d9ba0aff13297cb0e2
necemd <nurecemdilek@hotmail.com>
2016-04-25 00:23:38 +0300

    Locatable interface

--- app/src/main/java/com/mappers/campus101/models/Locatable.java
+++ app/src/main/java/com/mappers/campus101/models/Locatable.java
@@ -1,8 +1,14 @@
 package com.mappers.campus101.models;
 
 /**
- * @author Kadir Can Çelik
+ * @author Nur Ecem Dilek
  * @date 24.4.2016
  */
-public interface Locatable {
+public interface Locatable
+{
+    void setLocation ( double longitude, double latitude);
+    double getLongitude();
+    double getLatitude();
+    boolean isAtLocation( Location loc);
+
 }
